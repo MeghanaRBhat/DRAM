3da23dd70bd817dde860a1bc191d1638d5a35969
MeghanaRBhat <115388361+MeghanaRBhat@users.noreply.github.com>
2024-02-15 12:09:15 +0530

    Update dram_mon1.sv

--- dram_mon1.sv
+++ dram_mon1.sv
@@ -1,10 +1,16 @@
-class dram_drv extends uvm_driver #(dram_seq_item);
+class dram_mon1 extends uvm_monitor;
 
-`uvm_component_utils(dram_drv)
+`uvm_component_utils(dram_mon1)
+
+dram_seq_item pkt;
 
 virtual intif inf;
 
-function new(string name="dram_drv",uvm_component parent);
+uvm_analysis_port #(dram_seq_item) item_collected_port;
+
+uvm_analysis_port #(dram_seq_item) custom_ap;
+
+function new(string name="dram_mon1",uvm_component parent);
 
 super.new(name,parent);
 
@@ -14,13 +20,15 @@ virtual function void build_phase(uvm_phase phase);
 
 super.build_phase(phase);
 
-uvm_config_db #(virtual intif)::get(this,"","inf",inf);
+uvm_config_db #(virtual intif) ::get(this,"","inf",inf);
+
+item_collected_port=new("item_collected_port",this);
+
+custom_ap = new("analysis_port",this);
 
 endfunction
 
 task run_phase(uvm_phase phase);
-
-dram_seq_item pkt;
 
 pkt=dram_seq_item::type_id::create("pkt");
 
@@ -28,46 +36,35 @@ forever
 
 begin
 
-seq_item_port.get_next_item(pkt);
-
-pkt.en=1;
-
-@(negedge inf.clk);
-
-inf.en=pkt.en;
-
-if(pkt.wr==0) begin
-
-inf.wr0=pkt.wr;
-
-inf.data0_in=pkt.data_in;
-
-inf.add0=pkt.add;
-
-`uvm_info("DRV TRANSACTIONS", $sformatf("inf.data0_in=%d,inf.add0=%d, inf.wr0=%b",inf.data0_in,inf.add0,inf.wr0) ,UVM_NONE);
-
-end
-
-else begin
-
-inf.wr1=pkt.wr;
-
-inf.add1=pkt.add;
-
-`uvm_info("DRV TRANSACTIONS", $sformatf("inf.add1=%d, inf.wr1=%b",inf.add1,inf.wr1) ,UVM_NONE);
-
-end
-
-@(negedge inf.clk);
-
-seq_item_port.item_done();
-
-`uvm_info("DRV","DRV TRANSACTION TO DUT",UVM_NONE);
-
 #5;
+
+@(posedge inf.clk)
+
+if((inf.en==1)&&(inf.wr0==0)) begin
+
+pkt.en=inf.en;
+
+pkt.wr=inf.wr0;
+
+pkt.data_in=inf.data0_in;
+
+pkt.add=inf.add0;
+
+`uvm_info("MON1","MON1 TRANSACTIONS" ,UVM_NONE);
+
+end
+
+`uvm_info("MON","MON TRANSACTIONS",UVM_NONE);
+
+@(posedge inf.clk)
+
+item_collected_port.write(pkt);
+
+custom_ap.write (pkt);
 
 end
 
 endtask
 
 endclass
+
